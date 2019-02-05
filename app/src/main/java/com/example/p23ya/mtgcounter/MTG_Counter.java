4b5ca6695be94b7e727eb6533dd10f61156d897b
Gabriel Rizzolli Partichelli <gabrielpartichelli@gmail.com>
2019-02-05 17:37:46 -0200

    Initial commit, add basic layout features (buttons, health, background)

---
+++ app/src/main/java/com/example/p23ya/mtgcounter/MTG_Counter.java
@@ -0,0 +1,13 @@
+package com.example.p23ya.mtgcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MTG_Counter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mtg__counter);
+    }
+}
