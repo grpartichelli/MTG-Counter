52c0eac06dd1f76c190971e247a1a579674b6bcd
Gabriel Rizzolli Partichelli <gabrielpartichelli@gmail.com>
2019-02-10 23:25:29 -0200

    Add toolbar with reset buttom.

--- app/src/main/java/com/example/p23ya/mtgcounter/MTG_Counter.java
+++ app/src/main/java/com/example/p23ya/mtgcounter/MTG_Counter.java
@@ -1,27 +1,71 @@
 package com.example.p23ya.mtgcounter;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class MTG_Counter extends AppCompatActivity {
     //Health starts at 20
     private int playerOneHP=20;
     private int playerTwoHP=20;
+    private TextView playerOneHPTextView;
+    private TextView playerTwoHPTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mtg__counter);
 
+        //Finds and stores player one and two hp text views
+        playerOneHPTextView = (TextView) findViewById(R.id.player_one_hp_textview);
+        playerTwoHPTextView= (TextView) findViewById(R.id.player_two_hp_textview);
+
+        //Configure the toolbar
+        addToolbar();
+
         //Configure the plus and minus buttons.
         addButtons();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        //If the button clicked was the reset button, reset players HP
+        if (id == R.id.reset_HP_button) {
+
+            this.playerOneHP = 20;
+            this.playerTwoHP=20;
+
+            displayHP(playerOneHP,playerOneHPTextView);
+            displayHP(playerTwoHP,playerTwoHPTextView);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void  addToolbar(){
+        //Finds the toolbar and sets it as a replacement for the action bar
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
     protected void addButtons(){
 
         //Finds the minus button for player one
@@ -31,7 +75,6 @@ public class MTG_Counter extends AppCompatActivity {
             //When the button is pressed:
             public void onClick(View v) {
                 //Lowers player HP on click
-                TextView playerOneHPTextView= (TextView) findViewById(R.id.player_one_hp_textview);
                 lowerHP(playerOneHP, playerOneHPTextView);
 
             }
@@ -44,7 +87,6 @@ public class MTG_Counter extends AppCompatActivity {
             //When the button is pressed:
             public void onClick(View v) {
                 //Increases player HP on click
-                TextView playerOneHPTextView= (TextView) findViewById(R.id.player_one_hp_textview);
                 increaseHP(playerOneHP, playerOneHPTextView);
 
             }
@@ -59,7 +101,6 @@ public class MTG_Counter extends AppCompatActivity {
             //When the button is pressed:
             public void onClick(View v) {
                 //Lowers player HP on click
-                TextView playerTwoHPTextView= (TextView) findViewById(R.id.player_two_hp_textview);
                 lowerHP(playerTwoHP, playerTwoHPTextView);
 
             }
@@ -72,7 +113,6 @@ public class MTG_Counter extends AppCompatActivity {
             //When the button is pressed:
             public void onClick(View v) {
                 //Increases player HP on click
-                TextView playerTwoHPTextView= (TextView) findViewById(R.id.player_two_hp_textview);
                 increaseHP(playerTwoHP, playerTwoHPTextView);
 
             }
@@ -87,18 +127,12 @@ public class MTG_Counter extends AppCompatActivity {
 
         playerHP--;
 
-        //If a player HP goes 0 or lower, it becomes red.
-        if(playerHP <= 0 ) {
-            playerView.setTextColor(getResources().getColor(R.color.darkerRed, getResources().newTheme()));
-        }
-
-
 
         //Changes the textview
-        playerView.setText(Integer.toString(playerHP));
+        displayHP(playerHP,playerView);
 
         //Changes the outside variable accordingly to what textview is being changed
-        if(playerView == ((TextView) findViewById(R.id.player_one_hp_textview))){
+        if(playerView.getId() == R.id.player_one_hp_textview){
             this.playerOneHP = playerHP;
         }
         else{
@@ -116,18 +150,13 @@ public class MTG_Counter extends AppCompatActivity {
             playerHP++;
         }
 
-        //If player HP is higher then 0, it is white.
-        if(playerHP > 0 ){
-            playerView.setTextColor(getResources().getColor(R.color.colorWhite, getResources().newTheme()));
-        }
-
 
         //Changes the textview
-        playerView.setText(Integer.toString(playerHP));
+        displayHP(playerHP,playerView);
 
 
         //Changes the outside variable accordingly to what textview is being changed
-        if(playerView == ((TextView) findViewById(R.id.player_one_hp_textview))){
+        if(playerView.getId() == R.id.player_one_hp_textview){
             this.playerOneHP = playerHP;
         }
         else{
@@ -135,6 +164,22 @@ public class MTG_Counter extends AppCompatActivity {
         }
 
 
+    }
+
+    //Given a HP value and a textview, alters that textview to display the value
+    protected void displayHP( int playerHP, TextView playerView){
+
+
+        //If a player HP goes 0 or lower, it becomes red.
+        if(playerHP <= 0 ) {
+            playerView.setTextColor(getResources().getColor(R.color.darkerRed, getResources().newTheme()));
+        }
+        else{
+            //If player HP is higher then 0, it is white.
+            playerView.setTextColor(getResources().getColor(R.color.colorWhite, getResources().newTheme()));
+        }
+
+        playerView.setText(Integer.toString(playerHP));
     }
 
 
